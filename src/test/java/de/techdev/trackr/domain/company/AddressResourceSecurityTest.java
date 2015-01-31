3294da162a1c893015d3106cf0ba14b15c4b07cb
Alexander Hanschke <alexander.hanschke@gmail.com>
2015-01-31 13:10:57 +0100

    WIP: reactivate admin tests for address resource

--- src/test/java/de/techdev/trackr/domain/company/AddressResourceSecurityTest.java
+++ src/test/java/de/techdev/trackr/domain/company/AddressResourceSecurityTest.java
@@ -5,17 +5,15 @@ import de.techdev.trackr.domain.AbstractDomainResourceSecurityTest;
 import org.junit.Test;
 import org.springframework.test.context.jdbc.Sql;
 
-import javax.json.stream.JsonGenerator;
-import java.io.StringWriter;
-
-import static de.techdev.trackr.domain.DomainResourceTestMatchers2.isAccessible;
-import static de.techdev.trackr.domain.DomainResourceTestMatchers2.isMethodNotAllowed;
+import static de.techdev.trackr.domain.DomainResourceTestMatchers2.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @Sql("address/resourceTest.sql")
 @Sql(value = "address/resourceTestCleanup.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @OAuthToken("ROLE_ADMIN")
 public class AddressResourceSecurityTest extends AbstractDomainResourceSecurityTest {
+
+	private AddressJsonGenerator jsonGenerator = new AddressJsonGenerator();
 
     @Override
     protected String getResourceName() {
@@ -34,20 +32,22 @@ public class AddressResourceSecurityTest extends AbstractDomainResourceSecurityT
         assertThat(one(0L), isAccessible());
     }
 
-//    @Test
-//    public void createAllowedForAdmin() throws Exception {
-//        assertThat(create(adminSession()), isCreated());
-//    }
+    @Test
+    public void createAllowedForAdmin() throws Exception {
+		String json = jsonGenerator.start().build();
+		assertThat(create(json), isCreated());
+    }
 
-//    @Test
-//    public void putAllowedForAdmin() throws Exception {
-//        assertThat(update(adminSession()), isUpdated());
-//    }
+    @Test
+    public void putAllowedForAdmin() throws Exception {
+		String json = jsonGenerator.start().apply(c -> c.setId(0L)).build();
+		assertThat(update(0L, json), isUpdated());
+    }
 
-//    @Test
-//    public void patchAllowedForAdmin() throws Exception {
-//        assertThat(updateViaPatch(adminSession(), "{\"street\": \"test\"}"), isUpdated());
-//    }
+    @Test
+    public void patchAllowedForAdmin() throws Exception {
+        assertThat(updateViaPatch(0L, "{\"street\": \"test\"}"), isUpdated());
+    }
 
 //    @Test
 //    public void createNotAllowedForSupervisor() throws Exception {
@@ -69,19 +69,4 @@ public class AddressResourceSecurityTest extends AbstractDomainResourceSecurityT
         assertThat(remove(0L), isMethodNotAllowed());
     }
 
-    protected String getJsonRepresentation(Address address) {
-        StringWriter writer = new StringWriter();
-        JsonGenerator jg = jsonGeneratorFactory.createGenerator(writer);
-        jg.writeStartObject()
-          .write("street", address.getStreet())
-          .write("houseNumber", address.getHouseNumber())
-          .write("city", address.getCity())
-          .write("zipCode", address.getZipCode())
-          .write("country", address.getCountry());
-        if (address.getId() != null) {
-            jg.write("id", address.getId());
-        }
-        jg.writeEnd().close();
-        return writer.toString();
-    }
 }
